9bcd4572784313a909b9094e021b0dce647e976d
ludwinner <ludwingbravo4@gmail.com>
2022-11-19 12:03:17 -0300

    filtrar informacion

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,141 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import DetalleIngresadas from './api/DetalleIngresadas.js'
 import PreSolicitud from './api/PreSolicitud.js'
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
+// import Card from './components/Card.jsx';
+// import Cards from './components/Cards.jsx';
+// import SearchBar from './components/SearchBar.jsx';
 
 function App() {
   const [solicitud, setSolicitud] = useState([])
-  const [zonas, setZonas] = useState([])
-  const [equipo, setEquipo] = useState([])
-  const [supervisor, setSupervisor] = useState([])
-  const [vendedor, setVendedor] = useState([])
+  const info =  PreSolicitud
+  
+  const Team = (z) =>{
+    const preTeam = []
+    let preteam = info.map(ps => {
+      if (ps.NombreZona === z){ 
+        return ps.EsMiniEmprendedor
+      }
+    }).filter(fil => fil !== undefined)
+    preteam = preteam.filter((team,index)=>{
+      if (preteam.indexOf(team) === index){
+          const newTeams = {
+            "tipo": team,
+            "sups": [] 
+          }
 
-  const Coso = (z) =>{
-    // let detalleIngresadas = DetalleIngresadas
-    let prezona = PreSolicitud.map(ps => ps.NombreZona)
-    prezona = prezona.filter((item,index)=>{
-      return prezona.indexOf(item) === index;
+          newTeams.sups.push(Sups(team,z))
+          preTeam.push(newTeams)
+          return newTeams
+      }
     })
-    let preteam =  PreSolicitud.map(ps => ps.EsMiniEmprendedor)
-    preteam = preteam.filter((item,index)=>{
-      return preteam.indexOf(item) === index;
+    return preTeam
+  }
+  const Sups = (t,z) =>{
+    const preSups = []
+    let presups =  info.map(psu => {
+      if (psu.EsMiniEmprendedor === t){ 
+        return psu.NomSucursal
+      }
     })
-    let presups =  PreSolicitud.map(ps => ps.NomSucursal)
-    presups = presups.filter((item,index)=>{
-      return presups.indexOf(item) === index;
+    presups = presups.filter((sups,index)=>{
+      if (presups.indexOf(sups) === index){
+        if (sups!== undefined) {
+          const newSups = {
+            "name":sups,
+            "vend":[]
+          }
+          newSups.vend.push(Vendedor(sups,z))
+          preSups.push(newSups)
+        }
+      }
     })
-    let preven =  PreSolicitud
-    preven = preven.map(ps => {
-      delete ps.NomSucursal
-      // console.log(ps,"ps");
-      delete ps.NombreZona
-      delete ps.EsMiniEmprendedor
-      return ps
+    return preSups
+  }
+  const Vendedor = (su,z) =>{
+    let preVen = info.map(ven => {
+      if (ven.NomSucursal === su && ven.NombreZona === z) {
+        delete ven.NomSucursal
+        delete ven.NombreZona
+        delete ven.EsMiniEmprendedor
+        return ven
+      }
     })
-    
-    const newZones = {
-      "name":prezona,
-      "team":equipo 
-    }
-    const newTeams = {
-      "tipo": preteam,
-      "team":supervisor 
-    }
-    const newSups = {
-      "name":presups,
-      "team":vendedor 
-    }
-    const newVend = preven
-
-    setSolicitud(newZones)
-    // setZonas(newTeams)
-    // setEquipo(newSups)
-    // setSupervisor(newVend)
-    console.log(solicitud, 'PreSolicitud');
+    preVen = preVen.filter(pv => pv !== undefined)
+    return preVen
+  }
+  
+  const Solicitud = () =>{
+    let prezona = info.map(ps => ps.NombreZona)
+    prezona = prezona.filter((zone,index)=>{
+      if (prezona.indexOf(zone) ===index){
+        const newZones = {
+          "name":zone,
+          "team":[],
+          'key': zone
+        }
+        newZones.team.push(Team(zone))
+        setSolicitud(oldSolicitud => [...oldSolicitud, newZones]);
+      }
+    })
   }
   return (
     <div className="App">
-        <button onClick={Coso}>coso</button>
-      <hr />
-      {/* <div>
-        <Cards
-          zones={solicitud}
-        />
-      </div>
+      <button onClick={Solicitud}>coso</button>
       <hr />
       <div>
-        <SearchBar
-          onSearch={(z) => Coso(z)}
-        />
-      </div> */}
+        <table class="default">
+          <tr>
+            <td colspan='10'></td>
+            <td colspan='12'>CLASIFICACIONES PENDIENTES</td>
+            <td colspan='4'>MESES ANTERIORES</td>
+            <td colspan='1'></td>
+          </tr>
+          <tr>
+            <td colspan='3'>Vendedor</td>
+            <td colspan='1'>Fecha Alta</td>
+            <td colspan='1'>Fecha Baja</td>
+            <td colspan='1'>Ingresadas</td>
+            <td colspan='1'>Ventas MP</td>
+            <td colspan='1'>Cruce Scoring</td>
+            <td colspan='1'>Objetivo</td>
+            <td colspan='1'>Producción</td>
+
+            <td colspan='1'>2</td>
+            <td colspan='1'>4</td> 
+            <td colspan='1'>5</td>
+            <td colspan='1'>6</td>
+            <td colspan='1'>7</td>
+            <td colspan='1'>SUBTOTAL</td>	
+            <td colspan='1'>3</td>
+            <td colspan='1'>8</td>
+            <td colspan='1'>9</td>
+            <td colspan='1'>SUBTOTAL</td>	
+            <td colspan='1'>Anulada 3 + 7</td>
+            <td colspan='1'>Anulada Rechazada</td>
+
+            <td colspan='1'>-1</td>
+            <td colspan='1'>-2</td>
+            <td colspan='1'>-3</td>
+            <td colspan='1'>PROM</td>	
+            <td colspan='1'>GB</td>
+          </tr>
+          {
+            solicitud?.length>0 ?
+            solicitud?.length>0 && solicitud?.map((e)=>{
+              return (
+                <tr>
+              <td colspan='27'>{`${e.name}`}</td>
+        </tr>
+              )
+            }) 
+            :
+            <tr>
+            <td colspan='27' rowspan='10'>SIN DATOS</td>
+        </tr>
+          }
+        </table>
+      </div>
     </div>
   );
 }
